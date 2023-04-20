1a22307ca1a2774a4f0985910d21a97dc0e03688
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2023-04-20 13:56:55 +0700

    Update

--- Angular12/src/app/app.module.ts
+++ Angular12/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {FormsModule} from "@angular/forms";
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- Angular12/src/app/home/home.component.ts
+++ Angular12/src/app/home/home.component.ts
@@ -7,4 +7,8 @@ import {Component} from '@angular/core';
 })
 export class HomeComponent {
   public name = 'JayBee K';
+
+  public resetName() {
+    this.name = '';
+  }
 }
