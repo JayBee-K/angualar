fd6f22ad600a1ad0cbf4a285fc52608f9e046c26
Jay <killerdrug007@gmail.com>
2023-04-29 14:23:28 +0700

    Routing

--- Angular12/src/app/app-routing.module.ts
+++ Angular12/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {HomeComponent} from "./home/home.component";
+import {IndexComponent} from "./index/index.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent}, // Đường dẫn không có thì sẽ lấy ra component mặc định
+  {path: 'home', component: HomeComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- Angular12/src/app/app.module.ts
+++ Angular12/src/app/app.module.ts
@@ -7,12 +7,16 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import { OnSalePipe } from './on-sale.pipe';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        OnSalePipe
+        OnSalePipe,
+        PageNotFoundComponent,
+        IndexComponent
     ],
     imports: [
         BrowserModule,
